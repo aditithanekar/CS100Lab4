96abccb3029e94ffd7352ace5bfe2dafef971593
aditithanekar <75003679+aditithanekar@users.noreply.github.com>
2023-05-04 19:54:41 -0700

    Revert "Awards testing branch"

--- include/Awards.h
+++ include/Awards.h
@@ -17,10 +17,10 @@ public:
 class AwardCeremonyActions {
 public:
   virtual void playAnthem() = 0;
+  virtual void turnOffTheLightsAndGoHome() = 0;
   virtual void awardBronze(std::string recipient) = 0;
   virtual void awardSilver(std::string recipient) = 0;
   virtual void awardGold(std::string recipient) = 0;
-  virtual void turnOffTheLightsAndGoHome() = 0;
 };
 
 

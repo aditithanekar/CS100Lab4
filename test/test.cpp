5212e0539161701adf5a524ba87acd5ccc89eb82
aditithanekar <aditipthanekar@gmail.com>
2023-05-04 18:05:03 -0700

    cleaning up awards mock

--- test/test.cpp
+++ test/test.cpp
@@ -44,24 +44,3 @@ class MockAwardCeremonyActions:public AwardCeremonyActions
 };
 
 
-
-TEST(HailstoneTests, satisfyHailstoneZeroFalse)
-{
-    bool result = sequence::satisfiesHailstone(0);
-    EXPECT_FALSE(result);
-}
-TEST(HailstoneTests, satisfyHailstoneOne)
-{
-    bool result = sequence::satisfiesHailstone(1);
-    EXPECT_TRUE(result);
-}
-TEST(HailstoneTests, satisfyHailstoneEven)
-{
-    bool result = sequence::satisfiesHailstone(28);
-    EXPECT_TRUE(result);
-}
-TEST(HailstoneTests, satisfyHailstoneOdd)
-{
-    bool result = sequence::satisfiesHailstone(7);
-    EXPECT_TRUE(result);
-}
